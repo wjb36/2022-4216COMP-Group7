4d98f7ccc82968563bb15c6ad608d266da20b3af
Bill Byrne <billbyrne1@outlook.com>
2023-03-24 12:57:04 +0000

    main menu (has faults)

--- ui.py
+++ ui.py
@@ -1,15 +1,44 @@
+import math
+
 def mainMenu():
     print("Welcome To Earthquake Program")
-    print("Please Select An Option")
-    print("1. Create Line Chart")
-    print("2. Create Bar Chart")
-    print("3. Create Pie Chart")
-    print("4. Plot Earthquake Map Drawing")
-    print("5. Exit Program")
+    menuEnd = False
+    while menuEnd == False:
+        x = input("Please Select An Option:\n1. Create Line Chart\n2. Create Bar Chart\n3. Create Pie Chart\n4. Plot Earthquake Map Drawing\n5. Show Top 10\n6. Exit\n")
+        x = str(x)
+ 
+        if x == "1":
+            lineMenu()
+        elif x == "2":
+            barMenu()
+        elif x == "3":
+            pieMenu()
+        elif x == "4":
+            mapPlot()
+        elif x == "5":
+            top()
+        elif x == "6":
+            menuEnd = True
+        else:
+            print("Invalid")
+    
+    
+    
 def lineMenu():
-    print("Earthquake")
-def barMenu():
-    print("Earthquake")
+    print("Earthquake1")
 
-def hello():
-    print("Earthquake")
+def barMenu():
+    print("Earthquake2")
+
+def pieMenu():
+    print("Earthquake3")
+
+def mapPlot():
+    print("Earthquake4")
+ 
+def top():
+    print("Earthquake5")
+    
+
+
+mainMenu()
