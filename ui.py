6399c3caea664a67c3600a327f8a5c74bd89c5fd
Bill Byrne <billbyrne1@outlook.com>
2023-03-24 10:21:47 +0000

    Creation of UI File

---
+++ ui.py
@@ -0,0 +1,12 @@
+def mainMenu():
+    print("Welcome To Earthquake Program")
+    print("Please Select An Option")
+    print("1. Create Line Chart")
+    print("2. Create Bar Chart")
+    print("3. Create Pie Chart")
+    print("4. Plot Earthquake Map Drawing")
+    print("5. Exit Program")
+def lineMenu():
+    #Insert Code Here
+def barMenu():
+    #Insert Code Here
