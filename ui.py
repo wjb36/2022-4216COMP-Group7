8f1224f9ac44f8c798f680649c07e9d3e2fb281b
Bill <billbyrne1@outlook.com>
2023-03-26 14:07:21 +0100

    Pie Chart Main Menu Complete

--- ui.py
+++ ui.py
@@ -25,11 +25,11 @@ def mainMenu():
 def lineMenu():
     menuEnd = False
     while menuEnd == False:
-        x = input("Please Select An Option:\n1. Create Line Chart\n2. Create Bar Chart\n3. Create Pie Chart\n4. Plot Earthquake Map Drawing\n5. Show Top 10\n6. Exit\n")
+        x = input("Please Select An Option:\n1. Create Line Graph for Number of Earthquakes between two Specified Day\n2. Create Bar Chart\n3. Create Pie Chart\n4. Plot Earthquake Map Drawing\n5. Show Top 10\n6. Exit\n")
         x = str(x)
  
         if x == "1":
-            print("Earthquake2")
+            print("Create Line Graph for Number of Earthquakes between two Specified Day")
         elif x == "2":
             print("Earthquake2")
         elif x == "3":
@@ -47,20 +47,16 @@ def lineMenu():
 def barMenu():
     menuEnd = False
     while menuEnd == False:
-        x = input("Please Select An Option:\n1. Create Line Chart\n2. Create Bar Chart\n3. Create Pie Chart\n4. Plot Earthquake Map Drawing\n5. Show Top 10\n6. Exit\n")
+        x = input("Please Select An Option:\n1. Create Pie Chart to Show Percentage of True vs False for All Records\n2. Create Pie Chart to Show Percentage of Earthquakes with an Azimuthal Gap Below 180°\n3. Create Pie Chart to Show Percentage of Earthquakes based on Location\n4. Return\n")
         x = str(x)
  
         if x == "1":
-            print("Earthquake2")
+            print("Create Pie Chart to Show Percentage of True vs False for All Records")
         elif x == "2":
-            print("Earthquake2")
+            print("Create Pie Chart to Show Percentage of Earthquakes with an Azimuthal Gap Below 180°")
         elif x == "3":
-            print("Earthquake2")
+            print("Create Pie Chart to Show Percentage of Earthquakes based on Location")
         elif x == "4":
-            print("Earthquake2")
-        elif x == "5":
-            print("Earthquake2")
-        elif x == "6":
             menuEnd = True
             mainMenu()
         else:
@@ -69,20 +65,16 @@ def barMenu():
 def pieMenu():
     menuEnd = False
     while menuEnd == False:
-        x = input("Please Select An Option:\n1. Create Line Chart\n2. Create Bar Chart\n3. Create Pie Chart\n4. Plot Earthquake Map Drawing\n5. Show Top 10\n6. Exit\n")
+        x = input("Please Select An Option:\n1. Create Pie Chart to Show Percentage of True vs False for All Records\n2. Create Pie Chart to Show Percentage of Earthquakes with an Azimuthal Gap Below 180°\n3. Create Pie Chart to Show Percentage of Earthquakes based on Location\n4. Return\n")
         x = str(x)
  
         if x == "1":
-            print("Earthquake2")
+            print("Create Pie Chart to Show Percentage of True vs False for All Records")
         elif x == "2":
-            print("Earthquake2")
+            print("Create Pie Chart to Show Percentage of Earthquakes with an Azimuthal Gap Below 180°")
         elif x == "3":
-            print("Earthquake2")
+            print("Create Pie Chart to Show Percentage of Earthquakes based on Location")
         elif x == "4":
-            print("Earthquake2")
-        elif x == "5":
-            print("Earthquake2")
-        elif x == "6":
             menuEnd = True
             mainMenu()
         else:
