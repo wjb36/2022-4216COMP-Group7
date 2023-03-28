5664d85596fdb131dada60fb42696e6af511e79b
Bill <billbyrne1@outlook.com>
2023-03-28 18:04:10 +0100

    Menu Structure Complete & Refinements Made

--- ui.py
+++ ui.py
@@ -5,37 +5,44 @@ def mainMenu():
         menuInput = str(menuInput)
  
         if menuInput == "1":
+            print("You Have Selected To Display Line Charts For Your Data Query")
             lineMenu()
         elif menuInput == "2":
+            print("You Have Selected To Display Bar Charts For Your Data Query")
             barMenu()
         elif menuInput == "3":
+            print("You Have Selected To Display Pie Charts For Your Data Query")
             pieMenu()
         elif menuInput == "4":
-            mapPlot()
+            print("You Have Selected To Display A Map Plot For Your Data Query")
+            mapMenu()
         elif menuInput == "5":
-            top()
+            print("You Have Selected To Display A Ranking For Your Data Query")
+            rankingsMenu()
         elif menuInput == "6":
+            print("Goodbye")
             menuEnd = True
         else:
             print("Invalid")
     
     
 def lineMenu():
+    #Waiting For Elliot's Code File
     menuEnd = False
     while menuEnd == False:
-        menuInput = input("Please Select An Option:\n1. Create Line Graph for Number of Earthquakes between two Specified Day\n2. Create Bar Chart\n3. Create Pie Chart\n4. Plot Earthquake Map Drawing\n5. Show Top 10\n6. Exit\n")
+        menuInput = input("Please Select An Option:\n1. Create Line Graph for Number of Earthquakes between two Specified Day\n2. \n3. \n4. \n5. \n6. Return\n")
         menuInput = str(menuInput)
  
         if menuInput == "1":
             print("Create Line Graph for Number of Earthquakes between two Specified Day")
         elif menuInput == "2":
-            print("Earthquake2")
+            print("Option 2")
         elif menuInput == "3":
-            print("Earthquake2")
+            print("Option 3")
         elif menuInput == "4":
-            print("Earthquake2")
+            print("Option 4")
         elif menuInput == "5":
-            print("Earthquake2")
+            print("Option 5")
         elif menuInput == "6":
             menuEnd = True
             mainMenu()
@@ -43,17 +50,18 @@ def lineMenu():
             print("Invalid")
 
 def barMenu():
+    #Waiting For Elliot's Code File
     menuEnd = False
     while menuEnd == False:
-        menuInput = input("Please Select An Option:\n1. Create Pie Chart to Show Percentage of True vs False for All Records\n2. Create Pie Chart to Show Percentage of Earthquakes with an Azimuthal Gap Below 180°\n3. Create Pie Chart to Show Percentage of Earthquakes based on Location\n4. Return\n")
+        menuInput = input("Please Select An Option:\n1. \n2. \n3. \n4. Return\n")
         menuInput = str(menuInput)
  
         if menuInput == "1":
-            print("Create Pie Chart to Show Percentage of True vs False for All Records")
+            print("Option 1")
         elif menuInput == "2":
-            print("Create Pie Chart to Show Percentage of Earthquakes with an Azimuthal Gap Below 180°")
+            print("Option 2")
         elif menuInput == "3":
-            print("Create Pie Chart to Show Percentage of Earthquakes based on Location")
+            print("Option 3")
         elif menuInput == "4":
             menuEnd = True
             mainMenu()
@@ -61,6 +69,7 @@ def barMenu():
             print("Invalid")
 
 def pieMenu():
+    #Waiting For Elliot's Code File
     menuEnd = False
     while menuEnd == False:
         menuInput = input("Please Select An Option:\n1. Create Pie Chart to Show Percentage of True vs False for All Records\n2. Create Pie Chart to Show Percentage of Earthquakes with an Azimuthal Gap Below 180°\n3. Create Pie Chart to Show Percentage of Earthquakes based on Location\n4. Return\n")
@@ -78,32 +87,15 @@ def pieMenu():
         else:
             print("Invalid")
 
-def mapPlot():
+def mapMenu():
+    print("Map Plot")
+    mainMenu()
+ 
+def rankingsMenu():
+    #Waiting For Maaria's Code File
     menuEnd = False
     while menuEnd == False:
-        menuInput = input("Please Select An Option:\n1. Create Line Chart\n2. Create Bar Chart\n3. Create Pie Chart\n4. Plot Earthquake Map Drawing\n5. Show Top 10\n6. Exit\n")
-        menuInput = str(menuInput)
- 
-        if x == "1":
-            print("Earthquake2")
-        elif x == "2":
-            print("Earthquake2")
-        elif x == "3":
-            print("Earthquake2")
-        elif x == "4":
-            print("Earthquake2")
-        elif x == "5":
-            print("Earthquake2")
-        elif x == "6":
-            menuEnd = True
-            mainMenu()
-        else:
-            print("Invalid")
- 
-def top():
-    menuEnd = False
-    while menuEnd == False:
-        x = input("Please Select An Option:\n1. Create Line Chart\n2. Create Bar Chart\n3. Create Pie Chart\n4. Plot Earthquake Map Drawing\n5. Show Top 10\n6. Exit\n")
+        x = input("Please Select An Option:\n1. \n2. \n3. \n4. \n5. \n6. Return\n")
         x = str(x)
  
         if x == "1":
