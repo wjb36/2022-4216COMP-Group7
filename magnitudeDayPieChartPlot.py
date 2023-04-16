6b58595a505d25246a75afa556e6ad5bd8d174c4
Bill Byrne <billbyrne1@outlook.com>
2023-04-16 15:02:14 +0100

    Data Read & OOP Structure Implemented

--- magnitudeDayPieChartPlot.py
+++ magnitudeDayPieChartPlot.py
@@ -1,1 +1,23 @@
+import csv
 
+import matplotlib.pyplot as plt
+from datetime import date
+from datetime import datetime
+import matplotlib.ticker as ticker
+
+
+class earthquake:
+    earthquakes = []
+
+    with open('M:\earthquakeDataset.csv', 'r') as f:
+        csv_reader = csv.reader(f)
+        header_row = next(csv_reader)
+        print(header_row)
+
+        # each row is an earthquake
+        for row in csv_reader:
+            earthquakes.append(row)
+
+    def plotMagDayPie(earthquakes):
+        
+        
