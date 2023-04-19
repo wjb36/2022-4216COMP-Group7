ae89a11a790f2f194816549cf0f345f8470a9425
elliot-townley <123166481+elliot-townley@users.noreply.github.com>
2023-04-19 14:57:14 +0100

    added individual

---
+++ elliotIndividual.py
@@ -0,0 +1,44 @@
+
+import csv
+
+import matplotlib.pyplot as plt
+from datetime import date
+from datetime import datetime
+import matplotlib.ticker as ticker
+
+
+class earthquake:
+    earthquakes = []
+
+
+        
+
+    with open('E:\earthquakeDatasetElliot.csv', 'r') as f:
+        csv_reader = csv.reader(f)
+        header_row = next(csv_reader)
+        print(header_row)
+
+        # each row is an earthquake
+        for row in csv_reader:
+            
+            earthquakes.append(row)
+    
+       
+
+
+    def averageMonthPie(earthquakes):
+        pieData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        pieLabels = ["January", "Febuary", "March", "April", "May","June", "July", "August", "September", "October", "November", "December"]
+
+        for row in earthquakes:
+            Currentdate = datetime.strptime((row[0]), "%d-%b-%y").date()
+            month = Currentdate.month
+            IndexOfData = month - 1
+            pieData[month -1] = pieData[month -1] + 1
+
+        plt.pie(pieData, labels = pieLabels)
+        plt.legend()
+        plt.show()
+
+
+    averageMonthPie(earthquakes)
