b39b26a832a67a46170c1e4794a0eb869703c3ab
Joshua <Joshuakitts11@gmail.com>
2023-04-20 05:02:32 +0100

    changes

--- Josh.py
+++ Josh.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from datetime import date
 from datetime import datetime
 import matplotlib.ticker as ticker
+import decimal
 
 
 class earthquake:
@@ -21,140 +22,21 @@ class earthquake:
         for row in csv_reader:
             earthquakes.append(row)
     
-       
-
-    def lineTest(earthquakes):
-        print(earthquakes[0][0])
-
-        lat = []
-        long= []
-        
-
-        for a in earthquakes:
-            if a(11) == "TRUE":
-                lat.append(a[1])
-                long.append(a[2])
-
-        fig, ax = plt.subplots()
-
-        ax.bar(lat, long)
 
 
-        plt.show()
-
-    
-
-    def fiveDateBar(earthquakes):
-        usersDates = []
-        values = [0,0,0,0,0]
-
-        #asking user to input dates and formating them into a datetime
-        for i in range(5):
-            usersDate = (input("enter a date? (D-MMM-YY) "))
-            formatedDate = datetime.strptime(usersDate, "%d-%b-%y").date()
-            usersDates.append(formatedDate)
-
-        # matching users dates to files dates
-        for item in range(len(usersDates)):
-            for row in earthquakes:
-                Currentdate = datetime.strptime((row[0]), "%d-%b-%y").date()
-                if Currentdate == usersDates[item]:
-                    values[item] += 1
-        print(usersDates)
-
-        # Turns dates into a string
-        for h in range(len(usersDates)):
-           usersDates[h]=datetime.strftime(usersDates[h],"%d-%m-%y")
-
-        fig, ax = plt.subplots()
-        ax.bar(usersDates, values)
-
-        ax.set_xlabel('Entered Date')
-        ax.set_ylabel('Number of Magnitudes')
-        ax.set_title('Number of Earthquakes on Specified Date')
-
-        plt.show()
-
-
-
-    def magnitudetVsTime(earthquakes):
-        
-        mag = []
-        time = []
-
-
-        sortedEarthquakes = sorted(earthquakes, key=lambda x: x[5])
-
-
-        startDate = input("enter a start date? (D-MMM-YY)")
-        startDate = datetime.strptime(startDate, "%d-%b-%y").date()
-
-        endDate = input("enter an end date? (D-MMM-YY)")
-        endDate = datetime.strptime(endDate, "%d-%b-%y").date()
-
-        for row in sortedEarthquakes:
-            row_date = datetime.strptime(row[0], "%d-%b-%y").date()
-            if row_date >= startDate and row_date <= endDate:
-                if row[5] != "":
-                    mag.append(row[4])
-                    time.append(row[5])
-
-        
-        print(mag)
-
-        #mag, time = zip(*sorted(zip(mag, time)))
-
-        magTicks = sorted(mag)
-        timeTicks = sorted(time)
-
-        fig, ax = plt.subplots()
-        ax.plot(time, mag, 'ro')
-       
-        ax.set_xlabel('Time')
-        ax.set_ylabel('Magnitude')
-        ax.set_title('Magnitude vs Time')
-        plt.xticks(rotation = 45)
-
-
-        plt.show()
-
-
-
-
-    def trueFalsePie(earthquakes):
-        pieData = [0,0]
-        pieLabels = ["true", "false"]
+    def magniStatAzimuth(earthquakes):
+        magniStat = [0]
+        azimuth = [0]
 
         for row in earthquakes:
-            if row[11] == "TRUE":
-                pieData[0] = pieData[0]+ 1
-            elif row[11] == "FALSE":
-                pieData[1] = pieData[1] + 1
+            if row[6] != "":
+                magniStat.append(decimal.Decimal(row[6]))
+            if row[7] != "":
+                azimuth.append(decimal.Decimal(row[7]))
+        
+        fig, ax = plt.subplots()
+        ax.bar(magniStat, azimuth)
 
-        plt.pie(pieData, labels = pieLabels)
-        plt.legend()
-        plt.show() 
-
-    def dataSourcePie(earthquakes):
-        pieData = []
-        pieLabels = ["hi"]
-
-        for row in earthquakes:
-                if row[10] != "":
-                    for i in range(len(pieLabels)):
-                        if row[10] == i:
-                            pieData[pieLabels.index(i)]  = pieData[pieLabels.index(i)] + 1
-                        else:
-                            pieLabels.append(row[10])
-                            pieData.append(1)
-
-        print(pieData)
-        print(pieLabels)
-
-
-    #dataSourcePie(earthquakes)
-    #trueFalsePie(earthquakes)
-    #magnitudetVsTime(earthquakes)
-    fiveDateBar(earthquakes)
-    #lineTest(earthquakes)
-
+        plt.show()
+        
+    magniStatAzimuth(earthquakes)
