19b09bf4570817a00e273c01e0365afe5b5d6e13
“Joshua <joshuakitts11@gmail.com>
2023-04-20 05:46:06 +0100

    changes

--- Josh.py
+++ Josh.py
@@ -1,42 +1,77 @@
 import csv
 
+
+
+
 import matplotlib.pyplot as plt
+
 from datetime import date
+
 from datetime import datetime
+
 import matplotlib.ticker as ticker
+
 import decimal
 
 
+
+
+
 class earthquake:
+
     earthquakes = []
+    with open('M:\earthquakeDataset.csv', 'r') as f:
 
-
-        
-
-    with open('C:\earthquakeDataset.csv', 'r') as f:
         csv_reader = csv.reader(f)
+
         header_row = next(csv_reader)
+
         print(header_row)
 
-        # each row is an earthquake
+
+
+
+# each row is an earthquake
+
         for row in csv_reader:
+
             earthquakes.append(row)
-    
 
 
-    def magniStatAzimuth(earthquakes):
-        magniStat = [0]
-        azimuth = [0]
+
+
+
+    def magAz(earthquakes):
+
+        mag =[]
+
+        az = []
+
+
+
 
         for row in earthquakes:
+
             if row[6] != "" and row[7] != "":
-                magniStat.append(row[6])
-             
-                azimuth.append(row[7])
-        
+
+                mag.append(row[6])
+
+                az.append(row[7])
+
+
+
         fig, ax = plt.subplots()
-        ax.bar(magniStat, azimuth)
+
+
+        ax.bar(mag, az)
 
         plt.show()
-        
-    magniStatAzimuth(earthquakes)
+
+
+
+
+
+
+    magAz(earthquakes)
+
+
