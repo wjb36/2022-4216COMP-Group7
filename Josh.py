2834e0740381afb7901e91f6c8af36412b83b443
Joshua <Joshuakitts11@gmail.com>
2023-04-20 15:48:34 +0100

    changes

--- Josh.py
+++ Josh.py
@@ -1,52 +1,31 @@
 import csv
 
-
-
-
 import matplotlib.pyplot as plt
-
 from datetime import date
-
 from datetime import datetime
-
 import matplotlib.ticker as ticker
-
 import decimal
-
-
-
 
 
 class earthquake:
 
     earthquakes = []
-    with open('M:\earthquakeDataset.csv', 'r') as f:
-
+    with open('C:\earthquakeDataset.csv', 'r') as f:
         csv_reader = csv.reader(f)
-
         header_row = next(csv_reader)
-
         print(header_row)
 
 
 
-
 # each row is an earthquake
-
         for row in csv_reader:
-
             earthquakes.append(row)
 
 
 
-
-
     def magAz(earthquakes):
-
         mag =[]
-
         az = []
-
 
         startDate = input("enter a start date? (D-MMM-YY)")
         startDate = datetime.strptime(startDate, "%d-%b-%y").date()
@@ -57,25 +36,15 @@ class earthquake:
         for row in earthquakes:
             row_date = datetime.strptime(row[0], "%d-%b-%y").date()
             if row_date >= startDate and row_date <= endDate:
-
                 if row[6] != "" and row[7] != "":
-
                     mag.append(row[6])
-
                     az.append(row[7])
 
 
 
         fig, ax = plt.subplots()
-
-
         ax.bar(mag, az)
-
         plt.show()
-
-
-
-
 
 
     magAz(earthquakes)
